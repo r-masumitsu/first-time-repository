0cf218a69ebf8c1d316f76f8f53851de48affb87
益満 綾一 <r.masumitsu@masumitsunoMacBook-Air.local>
2024-10-09 22:45:56 +0900

    Dockerfile変更（SQL変更）

--- galaxy/src/main/resources/schema.sql
+++ galaxy/src/main/resources/schema.sql
@@ -1,21 +1,21 @@
-DROP TABLE IF EXISTS meeting_room;
-DROP TABLE IF EXISTS reservable_room;
-DROP TABLE IF EXISTS reservation;
-DROP TABLE IF EXISTS usr;
+DROP TABLE IF EXISTS meeting_room CASCADE;
+DROP TABLE IF EXISTS reservable_room CASCADE;
+DROP TABLE IF EXISTS reservation CASCADE;
+DROP TABLE IF EXISTS usr CASCADE;
 
-CREATE TABLE IF NOT EXISTS meeting_room (
+CREATE TABLE IF NOT EXISTS meeting_room(
     room_id SERIAL PRIMARY KEY,
     room_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS reservable_room (
+CREATE TABLE IF NOT EXISTS reservable_room(
     reserved_date DATE NOT NULL,
     room_id INT NOT NULL,
     PRIMARY KEY (reserved_date, room_id),
-    FOREIGN KEY (room_id) REFERENCES meeting_room(room_id)
+    FOREIGN KEY (room_id) REFERENCES meeting_room(room_id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS reservation (
+CREATE TABLE IF NOT EXISTS reservation(
     reservation_id SERIAL PRIMARY KEY,
     end_time TIME NOT NULL,
     start_time TIME NOT NULL,
@@ -24,11 +24,11 @@ CREATE TABLE IF NOT EXISTS reservation (
     user_id VARCHAR(255) NOT NULL,
     comment VARCHAR(255),
     online BOOLEAN NOT NULL,
-    FOREIGN KEY (reserved_date, room_id) REFERENCES reservable_room(reserved_date, room_id),
-    FOREIGN KEY (user_id) REFERENCES usr(user_id)
+    FOREIGN KEY (reserved_date, room_id) REFERENCES reservable_room(reserved_date, room_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES usr(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS usr (
+CREATE TABLE IF NOT EXISTS usr(
     user_id VARCHAR(255) PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
