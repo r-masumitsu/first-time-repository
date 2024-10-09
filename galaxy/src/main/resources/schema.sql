e2f5b703d08632f42331881eee974a7819194a8c
益満 綾一 <r.masumitsu@masumitsunoMacBook-Air.local>
2024-10-09 22:58:42 +0900

    Dockerfile変更（SQL変更）

--- galaxy/src/main/resources/schema.sql
+++ galaxy/src/main/resources/schema.sql
@@ -4,34 +4,34 @@ DROP TABLE IF EXISTS reservation CASCADE;
 DROP TABLE IF EXISTS usr CASCADE;
 
 CREATE TABLE IF NOT EXISTS meeting_room(
-    room_id SERIAL PRIMARY KEY,
-    room_name VARCHAR(255) NOT NULL
+room_id SERIAL PRIMARY KEY,
+room_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS reservable_room(
-    reserved_date DATE NOT NULL,
-    room_id INT NOT NULL,
-    PRIMARY KEY (reserved_date, room_id),
-    FOREIGN KEY (room_id) REFERENCES meeting_room(room_id) ON DELETE CASCADE
+reserved_date DATE NOT NULL,
+room_id INT NOT NULL,
+PRIMARY KEY (reserved_date, room_id),
+FOREIGN KEY (room_id) REFERENCES meeting_room(room_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS reservation(
-    reservation_id SERIAL PRIMARY KEY,
-    end_time TIME NOT NULL,
-    start_time TIME NOT NULL,
-    reserved_date DATE NOT NULL,
-    room_id INT NOT NULL,
-    user_id VARCHAR(255) NOT NULL,
-    comment VARCHAR(255),
-    online BOOLEAN NOT NULL,
-    FOREIGN KEY (reserved_date, room_id) REFERENCES reservable_room(reserved_date, room_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES usr(user_id) ON DELETE CASCADE
+reservation_id SERIAL PRIMARY KEY,
+end_time TIME NOT NULL,
+start_time TIME NOT NULL,
+reserved_date DATE NOT NULL,
+room_id INT NOT NULL,
+user_id VARCHAR(255) NOT NULL,
+comment VARCHAR(255),
+online BOOLEAN NOT NULL,
+FOREIGN KEY (reserved_date, room_id) REFERENCES reservable_room(reserved_date, room_id) ON DELETE CASCADE,
+FOREIGN KEY (user_id) REFERENCES usr(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS usr(
-    user_id VARCHAR(255) PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role_name VARCHAR(255) NOT NULL
+user_id VARCHAR(255) PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+role_name VARCHAR(255) NOT NULL
 );
