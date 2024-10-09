84dec5ff98aaf542b8f8b88bc771d5d25478917e
益満 綾一 <r.masumitsu@masumitsunoMacBook-Air.local>
2024-10-10 00:39:17 +0900

    Dockerfile変更（SQL変更）

--- galaxy/src/main/resources/schema.sql
+++ galaxy/src/main/resources/schema.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS reservable_room CASCADE;
 DROP TABLE IF EXISTS reservation CASCADE;
 DROP TABLE IF EXISTS usr CASCADE;
 
-CREATE TABLE IF NOT EXISTS meeting_room(roomid SERIAL PRIMARY KEY,room_name VARCHAR(255) NOT NULL);
+CREATE TABLE IF NOT EXISTS meeting_room(room_id SERIAL PRIMARY KEY,room_name VARCHAR(255) NOT NULL);
 
-CREATE TABLE IF NOT EXISTS reservable_room(reserved_date DATE NOT NULL,
+CREATE TABLE IF NOT EXISTS reservable_room(reserved_date DATE NOT NULL,¥
 room_id INT NOT NULL,
 PRIMARY KEY (reserved_date, room_id),
 FOREIGN KEY (room_id) REFERENCES meeting_room(room_id) ON DELETE CASCADE
