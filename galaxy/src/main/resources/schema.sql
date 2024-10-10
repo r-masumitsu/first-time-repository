2952bbee7ebec75a5949db2fe65e4551efd960b5
益満 綾一 <r.masumitsu@masumitsunoMacBook-Air.local>
2024-10-10 22:18:33 +0900

    Dockerfile変更（SQL変更）

--- galaxy/src/main/resources/schema.sql
+++ galaxy/src/main/resources/schema.sql
@@ -3,10 +3,39 @@ DROP TABLE IF EXISTS reservable_room CASCADE;
 DROP TABLE IF EXISTS reservation CASCADE;
 DROP TABLE IF EXISTS usr CASCADE;
 
-CREATE TABLE IF NOT EXISTS meeting_room(room_id SERIAL PRIMARY KEY, room_name VARCHAR(255) NOT NULL);
-
-CREATE TABLE IF NOT EXISTS reservable_room(reserved_date DATE NOT NULL, room_id INT NOT NULL, PRIMARY KEY (reserved_date, room_id), FOREIGN KEY (room_id) REFERENCES meeting_room(room_id) ON DELETE CASCADE);
-
-CREATE TABLE IF NOT EXISTS reservation(reservation_id SERIAL PRIMARY KEY, end_time TIME NOT NULL, start_time TIME NOT NULL, reserved_date DATE NOT NULL, room_id INT NOT NULL, user_id VARCHAR(255) NOT NULL, comment VARCHAR(255), online BOOLEAN NOT NULL, FOREIGN KEY (reserved_date, room_id) REFERENCES reservable_room(reserved_date, room_id) ON DELETE CASCADE, FOREIGN KEY (user_id) REFERENCES usr(user_id) ON DELETE CASCADE);
-
-CREATE TABLE IF NOT EXISTS usr(user_id VARCHAR(255) PRIMARY KEY, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role_name VARCHAR(255) NOT NULL);
+CREATE TABLE IF NOT EXISTS meeting_room (
+  room_id SERIAL NOT NULL,
+  room_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (room_id)
+);
+CREATE TABLE IF NOT EXISTS reservable_room (
+  reserved_date DATE NOT NULL,
+  room_id INT4 NOT NULL,
+  PRIMARY KEY (reserved_date, room_id)
+);
+CREATE TABLE IF NOT EXISTS reservation (
+  reservation_id SERIAL NOT NULL,
+  end_time TIME NOT NULL,
+  start_time TIME NOT NULL,
+  reserved_date DATE NOT NULL,
+  room_id INT4 NOT NULL,
+  user_id VARCHAR(255) NOT NULL,
+  comment VARCHAR(255) NOT NULL,
+  online BOOLEAN NOT NULL, 
+  PRIMARY KEY (reservation_id)
+);
+CREATE TABLE IF NOT EXISTS usr (
+  user_id VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (user_id)
+);
+-- 外部キー制約の削除および追加
+ALTER TABLE reservable_room DROP CONSTRAINT IF EXISTS FK_f4wnx2qj0d59s9tl1q5800fw7;
+ALTER TABLE reservation DROP CONSTRAINT IF EXISTS FK_p1k4iriqd4eo1cpnv79uvni9g;
+ALTER TABLE reservation DROP CONSTRAINT IF EXISTS FK_recqnfjcp370rygd9hjjxjtg;
+ALTER TABLE reservable_room ADD CONSTRAINT FK_f4wnx2qj0d59s9tl1q5800fw7 FOREIGN KEY (room_id) REFERENCES meeting_room;
+ALTER TABLE reservation ADD CONSTRAINT FK_p1k4iriqd4eo1cpnv79uvni9g FOREIGN KEY (reserved_date, room_id) REFERENCES reservable_room;
+ALTER TABLE reservation ADD CONSTRAINT FK_recqnfjcp370rygd9hjjxjtg FOREIGN KEY (user_id) REFERENCES usr;
