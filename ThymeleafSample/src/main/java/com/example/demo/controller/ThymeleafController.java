db3b7efe65f4edd267fb47dd3ef9626c35269946
益満 綾一 <r.masumitsu@masumitsunoMacBook-Air.local>
2024-10-16 23:23:31 +0900

    設定ファイル修正（502エラー対処）

--- ThymeleafSample/src/main/java/com/example/demo/controller/ThymeleafController.java
+++ ThymeleafSample/src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -4,15 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Member;
+
 @Controller
 public class ThymeleafController {
-	// ▽▽▽▽▽ リスト6.28 ▽▽▽▽▽
+	// ▽▽▽▽▽ リスト6.34 ▽▽▽▽▽
 	@GetMapping("show")
 	public String showView(Model model) {
+		// Memberを作成
+		Member member = new Member(1, "会員01");
 		// 「Model」にデータを格納する
 		model.addAttribute("name", "太郎");
+		model.addAttribute("mb", member);
 		// 戻り値は「ビュー名」を返す
 		return "main";
 	}
-	// △△△△△ リスト6.28 △△△△△
+	// △△△△△ リスト6.34 △△△△△
 }
