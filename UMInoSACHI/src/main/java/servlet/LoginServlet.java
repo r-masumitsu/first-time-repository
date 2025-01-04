3165e54de97068c188b364e5df7af7d591e5ae58
益満 綾一 <r.masumitsu@masumitsunoMacBook-Air.local>
2025-01-04 16:43:40 +0900

    既存WARファイルの上書き

--- UMInoSACHI/src/main/java/servlet/LoginServlet.java
+++ UMInoSACHI/src/main/java/servlet/LoginServlet.java
@@ -13,7 +13,7 @@ import model.Account;
 import model.Login;
 import model.LoginLogic;
 
-@WebServlet(urlPatterns = {"/login", ""})
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
